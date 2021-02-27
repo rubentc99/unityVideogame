ffbb0153fb019dd1e332ceb5bd01f12e3e740972
rubentc99 <rubentc99@hotmail.com>
2021-02-27 10:53:37 +0100

    Juego terminado

---
+++ thingsVSghosts/Assets/Scripts/SuperThings/CuboBasura.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuboBasura : MonoBehaviour
+{
+    public float frecuenciaDeBasuras = 1;
+    public GameObject Basura;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            //------------------------------------------------ APENAS INICIE EL JUEGO
+            yield return new WaitForSeconds(frecuenciaDeBasuras);
+            //------------------------------------------------ LANZA LA BASURA LUEGO DE LA FRECUENCIA
+            GameObject go = Instantiate(Basura, transform.position + Vector3.up *Random.Range(0f, 1f) + Vector3.left *Random.Range(-1f,1f) , Basura.transform.rotation) as GameObject;
+
+            //el 10 es el tiempo en el que se destruirá sola la basura
+            Destroy(go, 10);
+        }
+    }
+
+
+}
---
+++ thingsVSghosts/Assets/Scripts/SuperThings/MunnecoNieve.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MunnecoNieve : MonoBehaviour
+{
+
+    public float cadencia = 3;
+    public GameObject BolaDeNieve;
+    public Transform cañon;
+    public LayerMask layerFantasma;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            //------------------------------------------------ APENAS INICIE EL JUEGO
+            yield return new WaitForSeconds(cadencia);
+            //------------------------------------------------ LANZA LA BASURA LUEGO DE LA FRECUENCIA
+
+            //DISTANCIA DEL MUÑECO AL FANTASMA A LA QUE EMPIEZA A DISPARAR
+            RaycastHit2D hit = Physics2D.Raycast(cañon.position, Vector3.right, 50, layerFantasma);
+            if (hit.collider != null)
+            {
+                GameObject go = Instantiate(BolaDeNieve, cañon.position, BolaDeNieve.transform.rotation) as GameObject;
+                Destroy(go, 10);
+            }
+
+        }
+    }
+}
